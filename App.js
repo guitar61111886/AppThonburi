aa13cbc9ff5c5dcd9819c48c61c14e5b5a30200d
guitar61111886 <guitarthunrada@gmail.com>
2022-01-06 16:03:24 +0700

    เพิ่มเมนู Home กับ login

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ import Login from './screens/Login';
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <Home/> */}
+       <Home/> 
     <Detail/>
-    {/* <Login/> */}
+  <Login/>
     </View>
   );
 }
